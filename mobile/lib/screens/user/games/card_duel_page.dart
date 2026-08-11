644b9e809039e3808d6e11ccae866a3c7cc44cf7
siliksama-creator <siliksama@gmail.com>
2026-08-11 10:27:58 +0000

    Fix card duel Flutter number parsing

--- mobile/lib/screens/user/games/card_duel_page.dart
+++ mobile/lib/screens/user/games/card_duel_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart' as io;
 
 import '../../../api_client.dart';
+import '../../../core/assets.dart';
 import '../../../theme/colors.dart';
 import '../../../theme/tokens.dart';
 import '../../../widgets/app_card.dart';
